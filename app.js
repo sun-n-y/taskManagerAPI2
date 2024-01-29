d66983fbaed1a3aaf2bac1d84b31f32249502626
sunny patel <sunnykpatel.skp@gmail.com>
2024-01-29 16:11:28 -0500

    add connection to database

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const tasksRouter = require('./router/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
 //middleware
 app.use(express.json());
@@ -13,6 +15,15 @@ app.get('/hello', (req, res) => {
 app.use('/api/v1/tasks', tasksRouter);
 
 const port = 3000;
-app.listen(port, () => {
-  console.log(`Server listening to port ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server listening to port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
