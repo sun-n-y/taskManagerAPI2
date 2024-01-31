bc8a650b0cb34de392e05e50b76deb74b94b6f41
sunny patel <sunnykpatel.skp@gmail.com>
2024-01-30 20:57:25 -0500

    add custom error handling

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require('../models/Task');
 const asyncWrapper = require('../middleware/async');
+const { createCustomError } = require('../errors/custom-error');
 
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
@@ -13,11 +14,11 @@ const createTask = asyncWrapper(async (req, res) => {
   res.status(201).json({ msg: 'success, document created in mongodb', task });
 });
 
-const getSingleTask = asyncWrapper(async (req, res) => {
+const getSingleTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
   if (!task) {
-    return res.status(404).json({ msg: `no task with id: ${taskID}, exists` });
+    return next(createCustomError(`no task with id: ${taskID}, exists`, 404));
   }
   res.status(200).json({ msg: 'success, document retrieved', task });
 });
@@ -29,7 +30,7 @@ const updateTask = asyncWrapper(async (req, res) => {
     runValidators: true,
   });
   if (!task) {
-    return res.status(404).json({ msg: `no task with id: ${taskID}, exists` });
+    return next(createCustomError(`no task with id: ${taskID}, exists`, 404));
   }
   res.status(200).json({ id: taskID, task });
 });
@@ -38,7 +39,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
   if (!task) {
-    return res.status(404).json({ msg: `no task with id: ${taskID}, exists` });
+    return next(createCustomError(`no task with id: ${taskID}, exists`, 404));
   }
   res.status(200).json({ msg: 'success, task deleted', task });
 });
