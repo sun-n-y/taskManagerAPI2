1c6e554b0cc7aa651395b59dc47bfbb7756497e0
sunny patel <sunnykpatel.skp@gmail.com>
2024-01-30 21:02:39 -0500

    add port value alt

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use('/api/v1/tasks', tasksRouter);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
